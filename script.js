c73c67803ae5a4704246a4c4bc57fd5c581dc5f8
Sara Gilani <saragilani@Saras-MacBook-Pro.local>
2023-01-17 16:24:09 +0000

    delete function

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 const toDoListForm = document.querySelector('#toDoList-form');
 
 const handleToDoListForm = (event) => {
@@ -5,10 +6,34 @@ const handleToDoListForm = (event) => {
     event.preventDefault();
     const listItem = document.createElement("li");
     listItem.textContent = event.target.text.value;
-
+    
     const toDoList = document.querySelector("#list");
 
     toDoList.appendChild(listItem);
+
+    //adding delete
+    const button = document.createElement('button');
+    button.innerText = 'Delete';
+    button.addEventListener('click',() => deleteFunction(listItem));
+
+    listItem.appendChild(button);
+    
+
+    
 }
 
 toDoListForm.addEventListener('submit', handleToDoListForm);
+
+
+
+// //deleting something 
+
+const deleteFunction = (itemToDelete) => {
+    console.log("hello from delete");
+    const toDoList = document.querySelector("#list");
+    toDoList.removeChild(itemToDelete);
+}
+
+
+
+
